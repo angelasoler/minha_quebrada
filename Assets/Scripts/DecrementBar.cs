761bef68d03cd484c290da7345937348b7ed39aa
Thiago Leandro Liporace <85422792+thiliporace@users.noreply.github.com>
2023-11-26 04:04:09 -0300

    commit antes de dormir

--- Assets/Scripts/DecrementBar.cs
+++ Assets/Scripts/DecrementBar.cs
@@ -9,13 +9,32 @@ public class DecrementBar : MonoBehaviour
     public float current;
     public float initialValue;
 
+    public ConstructionsSpawn spawn;
+    private bool[][] arrayCasas;
+
     public void Start()
     {
         current = initialValue;
+        StartCoroutine(CalculateBarValue());
     }
     // Update is called once per frame
     public void Update()
     {
-        current -= Time.deltaTime * decrement;
+        arrayCasas = spawn.occupied;
+    
+    }
+
+    //Esboco inicial do codigo com sono :(, coloquei aqui porque o MainManager ja ta chamando ele, entao achei melhor fazer o calculo localmente mesmo, pode alterar dps sequiser
+    IEnumerator CalculateBarValue()
+    {
+  
+            for (int i = 0; i < arrayCasas.Length; i++)
+            {
+                decrement++;
+            }
+            current -= Time.deltaTime * (decrement / 10);
+            yield return new WaitForSeconds(UnityEngine.Random.Range(0,2));
+        
+
     }
 }
